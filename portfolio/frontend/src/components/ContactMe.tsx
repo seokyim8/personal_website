8656b143b0bb9d1696a2ef18c9a29d6b66aa6d14
Seok Yim <seokyim8@gmail.com>
2024-06-18 18:09:42 -0400

    Add images to projects

--- portfolio/frontend/src/components/ContactMe.tsx
+++ portfolio/frontend/src/components/ContactMe.tsx
@@ -7,7 +7,7 @@ const ContactMe = () => {
     });
 
     return (
-        <div id="projects" className="flex flex-col justify-center h-screen" ref={ref}>
+        <div id="projects" className="flex flex-col justify-center h-half" ref={ref}>
             <div className="font-semibold text-3xl flex flex-row justify-center">
                 Contact Me
             </div>
--- portfolio/frontend/src/components/Projects.tsx
+++ portfolio/frontend/src/components/Projects.tsx
@@ -14,13 +14,13 @@ const Projects = () => {
                 <div className="mx-auto text-xl pt-4 mb font-mono z-10">Take a look at my works!</div>
             </div>
 
-            <div className={"bg-slate-800 w-full max-w-5xl h-0.5 mx-auto " + (inView? "in-wipe-right-faster" : "")}/>
+            <div className={"bg-slate-800 w-full max-w-5xl h-0.5 mx-auto " + (inView ? "in-wipe-right-faster" : "")} />
 
             <div className="flex flex-row justify-center gap-8 flex-wrap max-w-7xl mx-auto pb-4 z-10">
                 <a href="#" className="flex flex-col justify-evenly bg-white shadow-md rounded-xl duration-500 
                 hover:scale-110 hover:shadow-xl h-96 w-96 pb-2">
                     <img src="/static/images/fox_image.png"
-                        alt="Product" className="h-80 object-cover rounded-t-xl" />
+                        alt="Product" className="h-80 object-cover rounded-t-xl p-2" />
                     <div className="px-4 py-2 flex flex-col">
                         <p className="text-lg font-bold text-black capitalize font-mono">Personal Website</p>
                         <span className="text-gray-400 mr-3 text-xs capitalize font-mono ">React/Js/Ts, Python, HTML/CSS, Tailwind</span>
@@ -28,8 +28,12 @@ const Projects = () => {
                 </a>
                 <a href="#" className="flex flex-col justify-evenly bg-white shadow-md rounded-xl duration-500 
                 hover:scale-110 hover:shadow-xl h-96 w-96 pb-2">
-                    <img src="/static/images/fox_image.png"
-                        alt="Product" className="h-80 object-cover rounded-t-xl" />
+                    <div className="flex felx-row justify-around h-80">
+                        <img src="https://github.com/seokyim8/Tetris/assets/49558316/2334edb3-d3af-4d02-a010-c4b349faee1a"
+                            alt="Product" className="object-cover rounded-3xl p-8 w-1/2" />
+                        <img src="https://github.com/seokyim8/Tetris/assets/49558316/00f62457-6435-44f3-bb47-b6f0df098025"
+                            alt="Product" className="object-cover rounded-3xl p-8 w-1/2" />
+                    </div>
                     <div className="px-4 py-2 flex flex-col">
                         <p className="text-lg font-bold text-black capitalize font-mono ">Self-learning Tetris AI</p>
                         <span className="text-gray-400 mr-3 text-xs capitalize font-mono ">Python, Pytorch, DQN, RL, ML, Neural Networks, Java</span>
@@ -37,8 +41,8 @@ const Projects = () => {
                 </a>
                 <a href="#" className="flex flex-col justify-evenly bg-white shadow-md rounded-xl duration-500 
                 hover:scale-110 hover:shadow-xl h-96 w-96 pb-2">
-                    <img src="/static/images/fox_image.png"
-                        alt="Product" className="h-80 object-cover rounded-t-xl" />
+                    <img src="/static/images/steam_data_pipeline.png"
+                        alt="Product" className="h-80 object-cover rounded-t-xl p-4" />
                     <div className="px-4 py-2 flex flex-col">
                         <p className="text-lg font-bold text-black capitalize font-mono ">Steam Data Pipeline</p>
                         <span className="text-gray-400 mr-3 text-xs capitalize font-mono ">Python, SQL, Docker, AWS, Selenium, Apache-superset</span>
@@ -46,8 +50,10 @@ const Projects = () => {
                 </a>
                 <a href="#" className="flex flex-col justify-evenly bg-white shadow-md rounded-xl duration-500 
                 hover:scale-110 hover:shadow-xl h-96 w-96 pb-2">
-                    <img src="/static/images/fox_image.png"
-                        alt="Product" className="h-80 object-cover rounded-t-xl" />
+                    <div className="flex flex-col justify-center h-80">
+                        <img src="/static/images/tksm.png"
+                            alt="Product" className="w-100 object-cover rounded-t-xl p-2" />
+                    </div>
                     <div className="px-4 py-2 flex flex-col">
                         <p className="text-lg font-bold text-black capitalize font-mono ">Social Media Website for TEKKEN 8</p>
                         <span className="text-gray-400 mr-3 text-xs capitalize font-mono ">Python, React.js, Django, Github Action, AWS</span>
@@ -55,8 +61,8 @@ const Projects = () => {
                 </a>
                 <a href="#" className="flex flex-col justify-evenly bg-white shadow-md rounded-xl duration-500 
                 hover:scale-110 hover:shadow-xl h-96 w-96 pb-2">
-                    <img src="/static/images/fox_image.png"
-                        alt="Product" className="h-80 object-cover rounded-t-xl" />
+                    <img src="/static/images/black_knight.png"
+                        alt="Product" className="h-80 object-cover rounded-t-xl p-24" />
                     <div className="px-4 py-2 flex flex-col">
                         <p className="text-lg font-bold text-black capitalize font-mono ">Chess on Android</p>
                         <span className="text-gray-400 mr-3 text-xs capitalize font-mono ">Java, Android Studio</span>
@@ -64,8 +70,8 @@ const Projects = () => {
                 </a>
                 <a href="#" className="flex flex-col justify-evenly bg-white shadow-md rounded-xl duration-500 
                 hover:scale-110 hover:shadow-xl h-96 w-96 pb-2">
-                    <img src="/static/images/fox_image.png"
-                        alt="Product" className="h-80 object-cover rounded-t-xl" />
+                    <img src="/static/images/photos_app.png"
+                        alt="Product" className="h-80 object-cover rounded-t-xl p-2" />
                     <div className="px-4 py-2 flex flex-col">
                         <p className="text-lg font-bold text-black capitalize font-mono ">Photo Management App</p>
                         <span className="text-gray-400 mr-3 text-xs capitalize font-mono ">Java, JavaFX11</span>
